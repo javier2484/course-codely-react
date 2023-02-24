54bd0a619d0337ffbfc99a13045bdf80ac59dd67
Javier Gonzalez <javier.gonzalez@thecolvinco.com>
2023-02-24 10:35:35 +0100

    config dash

--- src/devdash_config.ts
+++ src/devdash_config.ts
@@ -13,5 +13,9 @@ export const config: DevDashConfig = {
 			id: "228d51b7-713a-42d3-a79c-662d3a9d8b19",
 			repository_url: "https://github.com/javier2484/course-codely-react",
 		},
+		{
+			id: "d15f358e-6501-479a-a6ea-42e33bf0b09a",
+			repository_url: "https://github.com/javier2484/skeleton-ddd-typescript",
+		},
 	],
 };
