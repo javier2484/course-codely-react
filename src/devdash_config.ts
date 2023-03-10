4a010a85280e14efd495694c242600fa4d5ef4b3
Javier Gonzalez <javier.gonzalez@thecolvinco.com>
2023-03-10 10:30:05 +0100

    clase 4

--- src/devdash_config.ts
+++ src/devdash_config.ts
@@ -13,5 +13,9 @@ export const config: DevDashConfig = {
 			id: "228d51b7-713a-42d3-a79c-662d3a9d8b19",
 			repository_url: "https://github.com/javier2484/course-codely-react",
 		},
+		{
+			id: "5e524353-7e52-4581-8c1c-b5db1b593fc7",
+			repository_url: "https://github.com/javier2484/skeleton-ddd-typescript",
+		},
 	],
 };
--- src/sections/dashboard/Dashboard.tsx
+++ src/sections/dashboard/Dashboard.tsx
@@ -1,18 +1,32 @@
-import { config } from "../../devdash_config";
+import { githubApiResponses } from "../../git_hub_api_responses";
+import style from "./Dashboard.module.scss";
 
 export function Dashboard() {
 	const title = "DevDash_";
 
 	return (
 		<>
-			<header>
+			<header className={style.container}>
 				<h1>{title}</h1>
 			</header>
 
-			<section>
+			<section className={style.container}>
 				<ul>
-					{config.widgets.map((widget) => (
-						<li key={widget.id}>{widget.repository_url}</li>
+					{githubApiResponses.map((widget) => (
+						<article className={style.widget} key={widget.repositoryData.id}>
+							<header className={style.widget__header}>
+								<a
+									className={style.widget__title}
+									target="_blank"
+									href={widget.repositoryData.url}
+									title={`${widget.repositoryData.organization.login}/${widget.repositoryData.name}`}
+									rel="noreferrer"
+								>
+									{widget.repositoryData.organization.login}/{widget.repositoryData.name}
+								</a>
+								{/*{widget.repositoryData.private ? <Lock /> : <Unlock />}*/}
+							</header>
+						</article>
 					))}
 				</ul>
 			</section>
